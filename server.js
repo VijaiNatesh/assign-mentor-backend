dc09ef09934244280ce55bee581d1c6f2a125ce2
VijaiNatesh <87690836+VijaiNatesh@users.noreply.github.com>
2021-12-31 18:53:46 +0500

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ const studentRoute = require("./routes/studentRoute")
 const assignRoute = require("./routes/assignRoute")
 
 app.use(cors())
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 
