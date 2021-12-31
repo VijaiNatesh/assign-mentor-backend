8628a5b1818b5365aaa292f20a2431f83d23f8ab
VijaiNatesh <87690836+VijaiNatesh@users.noreply.github.com>
2021-12-31 18:57:31 +0500

    Update server.js

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ const studentRoute = require("./routes/studentRoute")
 const assignRoute = require("./routes/assignRoute")
 
 app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req,res) => {
     res.send("Mentor and Student")
