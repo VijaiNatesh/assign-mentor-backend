b14b53428f108eadcedcbc4e6cacc89bd24603e3
VijaiNatesh <87690836+VijaiNatesh@users.noreply.github.com>
2021-12-31 17:14:54 +0500

    Update assignRoute.js

--- routes/assignRoute.js
+++ routes/assignRoute.js
@@ -5,15 +5,15 @@ const Mentor = require("../models/mentor")
 const Student = require("../models/student")
 
 assignRoute.post('/studenttomentor', async(req, res) => {
-    const mentor = await Mentor.findById(req.body.mentorId)
+    const mentor = await Mentor.findById(ObjectId(req.body.mentorId))
     mentor.students = [
         ...mentor.students,
-        ...req.body.students
+        ...ObjectId(req.body.students)
     ]
     mentor.save();
     req.body.students.forEach(async(student) => {
         const assign = await Student.findById(student)
-        assign.mentor = req.body.mentorId
+        assign.mentor = ObjectId(req.body.mentorId)
         assign.save()
     })
     res.json(mentor)
