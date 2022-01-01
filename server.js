bce587fb67f1ce736a2c4de2eaf2a858baf10c2f
VijaiNatesh <87690836+VijaiNatesh@users.noreply.github.com>
2022-01-01 22:10:22 +0500

    Update server.js

--- server.js
+++ server.js
@@ -7,9 +7,18 @@ const mentorRoute = require("./routes/mentorRoute")
 const studentRoute = require("./routes/studentRoute")
 const assignRoute = require("./routes/assignRoute")
 
+var allowedOrigins = ['http://localhost:3000'];
 
 app.use(cors({
-    origin : "http://localhost:3000"
+   origin: function(origin, callback){ 
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
 }))
 app.options('*',cors())
 
