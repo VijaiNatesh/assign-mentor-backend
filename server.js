69ddb003d7550b9e1099554b45fb2d3817058dbf
VijaiNatesh <87690836+VijaiNatesh@users.noreply.github.com>
2022-01-01 19:38:48 +0500

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get("/", (req,res) => {
     res.send("Mentor and Student")
 })
 
-app.get("/", cors() (req, res) => {
+app.get("/", cors(), (req, res) => {
 res.setHeader("Access-Control-Allow-Origin", "*")
 res.setHeader("Access-Control-Allow-Credentials", "true");
 res.setHeader("Access-Control-Allow-Headers", "content-type");
