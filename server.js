846cb8e0e6afa7866a83f319b842c8c20158105d
VijaiNatesh <87690836+VijaiNatesh@users.noreply.github.com>
2022-01-01 18:52:54 +0500

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,12 @@ require('./config/dbConnect')();
 const mentorRoute = require("./routes/mentorRoute")
 const studentRoute = require("./routes/studentRoute")
 const assignRoute = require("./routes/assignRoute")
-
-app.use(cors());
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,           
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 //Enable cors
