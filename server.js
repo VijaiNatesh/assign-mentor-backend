b0398b5956e0f4c4dcd055ee83faa626ae0bb453
VijaiNatesh <87690836+VijaiNatesh@users.noreply.github.com>
2022-01-01 22:20:03 +0500

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ const mentorRoute = require("./routes/mentorRoute")
 const studentRoute = require("./routes/studentRoute")
 const assignRoute = require("./routes/assignRoute")
 
-var allowedOrigins = ['http://localhost:3000'];
-
 app.use(cors())
 app.use((req, res, next) => {
    res.header('Access-Control-Allow-Origin', '*');
@@ -21,13 +19,8 @@ app.use((req, res, next) => {
    }
    next();
 });
-app.options('*',cors())
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+app.options('*',cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
