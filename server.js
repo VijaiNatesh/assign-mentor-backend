6e621ade63b537263deb9bfbf10c64bf929fd53a
VijaiNatesh <87690836+VijaiNatesh@users.noreply.github.com>
2022-01-01 21:02:37 +0500

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(function(req, res, next) {
 });
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req,res) => {
     res.send("Mentor and Student")
