079d53f397677f3892fdc127f43a898c9e736f08
VijaiNatesh <87690836+VijaiNatesh@users.noreply.github.com>
2022-01-01 22:04:29 +0500

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const studentRoute = require("./routes/studentRoute")
 const assignRoute = require("./routes/assignRoute")
 
 
-app.use(cors())
+app.use(cors({
+    origin : "http://localhost:3000"
+}))
 app.options('*',cors())
 
 // app.use(function(req, res, next) {
