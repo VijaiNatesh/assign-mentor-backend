aec4dffd93512f859349e6491f44be4db3f7e39d
VijaiNatesh <87690836+VijaiNatesh@users.noreply.github.com>
2022-01-01 20:19:31 +0500

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const assignRoute = require("./routes/assignRoute")
 
 app.use(cors(), function(req, res, next){
     res.setHeader('Access-Control-Allow-Origin', '*');
+     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type');
+     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
 
