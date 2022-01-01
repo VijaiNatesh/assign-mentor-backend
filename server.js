3364d296b4698ef6e2830e48eefccb2b6e2252b7
VijaiNatesh <87690836+VijaiNatesh@users.noreply.github.com>
2022-01-01 18:30:13 +0500

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const assignRoute = require("./routes/assignRoute")
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 //Enable cors
-app.use(function(req, res, next) {
+app.use(cors(), function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); 
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
