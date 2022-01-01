5538aebe67b742be82a7a9205a87d325da39ad59
VijaiNatesh <87690836+VijaiNatesh@users.noreply.github.com>
2022-01-01 18:58:48 +0500

    Update server.js

--- server.js
+++ server.js
@@ -14,17 +14,21 @@ const corsOptions ={
 app.use(cors(corsOptions));
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
-//Enable cors
-// app.use(cors(), function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*"); 
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+
 
 
 app.get("/", (req,res) => {
     res.send("Mentor and Student")
 })
+
+router.get("/", (req, res) => {
+res.setHeader("Access-Control-Allow-Origin", "*")
+res.setHeader("Access-Control-Allow-Credentials", "true");
+res.setHeader("Access-Control-Max-Age", "1800");
+res.setHeader("Access-Control-Allow-Headers", "content-type");
+res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+ });
+
 app.use("/api/mentor", mentorRoute)
 app.use("/api/student", studentRoute)
 app.use("/api/assign", assignRoute)
