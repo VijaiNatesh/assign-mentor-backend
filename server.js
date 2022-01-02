1fa1d8edc7b134d467393c3c88a531070740c5a3
VijaiNatesh <87690836+VijaiNatesh@users.noreply.github.com>
2022-01-02 08:07:25 +0500

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use(express.json())
-app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req,res) => {
     res.send("Mentor and Student")
