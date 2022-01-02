fb82fe2ba11b80bf939ff0599f9307ec8e1f9223
VijaiNatesh <87690836+VijaiNatesh@users.noreply.github.com>
2022-01-02 07:41:18 +0500

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use((req, res, next) => {
    res.header('Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept, Authorization'
    );
+   res.header('Access-Control-Allow-Credentials': true);
    if (req.method === 'OPTIONS') {
       req.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
       return res.status(200).json({});
